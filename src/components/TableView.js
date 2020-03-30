2860b521933f3ea1e35711dac25239dc662762b8
Jace E <jace.egenbacher@ttu.edu>
2020-03-30 13:33:55 -0500

    Added basic table component

---
+++ src/components/TableView.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+const TableView = ({cups}) => {
+
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">TableID</TableCell>
+                <TableCell align="right">CupID</TableCell>
+                <TableCell align="right">Level</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {cups.map((cup) => (
+                <TableRow key={cup.tableid}>
+                   <TableCell component="th" scope="row" align = "right"> {cup.tableid} </TableCell>
+                  <TableCell align="right">{cup.cupid}</TableCell>
+                  <TableCell align="right">{cup.level}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+};
+
+export default TableView;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import TableView from "./components/TableView";
+import './App.css'
 
-class TableView extends Component {
+class Trex extends Component {
+
+    state = {
+        //fake data for now
+        cups: [{tableid : 1, cupid : 1, level : .2},
+                {tableid : 1, cupid : 2, level : .2},
+                {tableid : 12, cupid : 1, level : .2},
+                {tableid : 23, cupid : 1, level : .2}]
+    };
+
     render(){
         return(
-            <div className="title">Tables</div>
+            <div className="container">
+                <h1 className="title">Trex </h1>
+                    
+                     <TableView cups = {this.state.cups}  />
+    
+            </div>
         );
     }
 }
 
-ReactDOM.render(<TableView />, document.getElementById("root"));
+ReactDOM.render(<Trex />, document.getElementById("root"));
