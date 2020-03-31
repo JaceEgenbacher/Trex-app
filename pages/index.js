15f7bf37326b76b6fe2d6c62cf5e164db38756d7
Jace E <jace.egenbacher@ttu.edu>
2020-03-31 14:31:47 -0500

    removed GridContainer

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
-import GridContainer from '../components/GridContainer';
+import Grid from '@material-ui/core/Grid';
+import TableView from '../components/TableView';
+import Table from '../components/Table';
 
-export default GridContainer;
+const Index = () => {
+  return (
+    <Grid container spacing={0}>
+      <Grid item xs={9}>
+        <Table></Table>
+      </Grid>
+      <Grid item xs={3}>
+        <TableView></TableView>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Index;
