138db3a5ae8bf89f4d55c9ecc02737dc5cd5efb0
Jace E <jace.egenbacher@ttu.edu>
2020-03-31 15:05:21 -0500

    added fake api

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,22 @@ import Grid from '@material-ui/core/Grid';
 import TableView from '../components/TableView';
 import Table from '../components/Table';
 
+import { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+import { startClock } from '../store';
+
 const Index = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let timer = startClock(dispatch);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <Grid container spacing={0}>
       <Grid item xs={9}>
--- store.js
+++ store.js
@@ -2,31 +2,23 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 
+import _ from 'lodash';
+
 const theInitialState = {
-  tables: [
-    {
-      id: 1,
-      x: 0,
-      y: 0,
-      cups: [
-        { id: 1, level: 0.2 },
-        { id: 2, level: 0.2 },
-      ],
-    },
-    {
-      id: 2,
-      x: 100,
-      y: 100,
-      cups: [
-        { id: 3, level: 0.2 },
-        { id: 4, level: 0.2 },
-      ],
-    },
-  ],
+  tables: [],
+};
+
+export const actionTypes = {
+  UPDATE_TABLE: 'UPDATE_TABLE',
 };
 
 export const reducer = (state = exampleInitialState, action) => {
   switch (action.type) {
+    case actionTypes.UPDATE_TABLE:
+      return {
+        ...state,
+        tables: action.tables,
+      };
     default:
       return state;
   }
@@ -38,4 +30,44 @@ export function initializeStore(initialState = theInitialState) {
     initialState,
     composeWithDevTools(applyMiddleware(thunkMiddleware)),
   );
+}
+
+export const startClock = (dispatch) => {
+  return setInterval(async () => {
+    try {
+      const { tables } = await fakeApiCall();
+      dispatch({ type: actionTypes.UPDATE_TABLE, tables });
+    } catch (err) {
+      console.error(err);
+    }
+  }, 1000);
+};
+
+function fakeApiCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        tables: [
+          {
+            id: 1,
+            x: 0,
+            y: 0,
+            cups: [
+              { id: 1, level: _.random(0.0, 1.0, true) },
+              { id: 2, level: _.random(0.0, 1.0, true) },
+            ],
+          },
+          {
+            id: 2,
+            x: 100,
+            y: 100,
+            cups: [
+              { id: 3, level: _.random(0.0, 1.0, true) },
+              { id: 4, level: _.random(0.0, 1.0, true) },
+            ],
+          },
+        ],
+      });
+    }, 250);
+  });
 }
